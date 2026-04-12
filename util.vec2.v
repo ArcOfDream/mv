096241b9fcf491caada7a2ca6c3b6b7e67f957d9
garo <garo@vp.pl>
2026-04-12 11:10:44 +0000

    add some of the raylib vector operations

--- util.vec2.v
+++ util.vec2.v
@@ -9,6 +9,52 @@ pub const vec2_one = Vec2{1, 1}
 pub const vec2_unit_x = Vec2{1, 0}
 pub const vec2_unit_y = Vec2{0, 1}
 
+pub fn Vec2.f32(f f32) Vec2 {
+	return Vec2{f, f}
+}
+
+pub fn (v Vec2) length() f32 {
+	return rm.vector2_length(v)
+}
+
+pub fn (v Vec2) length_sqr() f32 {
+	return rm.vector2_length_sqr(v)
+}
+
+pub fn (v Vec2) dot(v2 Vec2) f32 {
+	return rm.vector_2dot_product(v, v2)
+}
+
+pub fn (v Vec2) cross(v2 Vec2) f32 {
+	return rm.vector2_cross_product(v, v2)
+}
+
+pub fn (v Vec2) distance(v2 Vec2) f32 {
+	return rm.vector_2distance(v, v2)
+}
+
+pub fn (v Vec2) distance_sqr(v2 Vec2) f32 {
+	return rm.vector_2distance_sqr(v, v2)
+}
+
+pub fn (v Vec2) angle(v2 Vec2) f32 {
+	return rm.vector2_angle(v, v2)
+}
+
+pub fn (v Vec2) line_angle(v2 Vec2) f32 {
+	return rm.vector2_line_angle(v, v2)
+}
+
+pub fn (v Vec2) scale(f f32) Vec2 {
+	result := rm.vector2_scale(v, f)
+	return Vec2{result.x, result.y}
+}
+
+pub fn (v Vec2) normalize() Vec2 {
+	result := rm.vector2_normalize(v)
+	return Vec2{result.x, result.y}
+}
+
 pub fn (a Vec2) + (b Vec2) Vec2 {
 	return Vec2{a.x + b.x, a.y + b.y}
 }
@@ -29,7 +75,7 @@ pub fn (a Vec2) / (b Vec2) Vec2 {
 }
 
 pub fn (a Vec2) == (b Vec2) bool {
-	return rm.float_equals(a.x, b.x) == 1 && rm.float_equals(a.y, b.y) == 1
+	return rm.float_equals(a.x, b.x) != 0 && rm.float_equals(a.y, b.y) != 0
 }
 
 pub fn (a Vec2) < (b Vec2) bool {
